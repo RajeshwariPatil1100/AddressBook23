a5f49706febccd09f26bd512386ce612238631ae
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-29 11:33:40 +0530

    [Rajeshwari] create contact in address book

--- AddressBook23/Program.cs
+++ AddressBook23/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AddressBook23;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address BOOK Program");
+            AddressBook ab = new AddressBook("Rajeshwari", "Patil", "mumbai", "Mumbai", "Maharashtra", "Rajeshwari@11.com", 400023, 9632587412);
+            ab.DisplayContacts();
+        }
+    }
+}
